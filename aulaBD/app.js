e032047bbe4c8202750e0f89bb54a774028778c5
saraqwe123 <130059301+saraqwe123@users.noreply.github.com>
2025-05-05 11:17:18 -0300

    ggg

--- aulaBD/app.js
+++ aulaBD/app.js
@@ -35,7 +35,6 @@ app.post('/cadastrarUsuario', function(req, res){
 
 });
 
-<<<<<<< HEAD
 app.post('/desativar/:username', function(req, res){
   const usuario = req.params.username
   const resultado = usuarioController.desativarUsuario(usuario)
@@ -47,9 +46,9 @@ app.post('/desativar/:username', function(req, res){
 
 
 app.post('/desativar', usuarioController.validandoDesativacao);
-=======
+
 app.get('/desativar', (req, res) => usuarioController.validandoDesativacao());
->>>>>>> 511eadf61e7fe28545db84c62e2aa234091df3aa
+
 
 
 app.listen(port, () => {
